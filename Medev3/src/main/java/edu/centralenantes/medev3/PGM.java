c2d3fb5b4184f3e635116b50f8c5d9416a6146ed
Louis Pautrat <louis.pautrat@eleves.ec-nantes.fr>
2021-12-02 09:56:40 +0100

    ajout d'un constructeur de recopie et de la fonction de seuillage

--- Medev3/src/main/java/edu/centralenantes/medev3/PGM.java
+++ Medev3/src/main/java/edu/centralenantes/medev3/PGM.java
@@ -44,5 +44,35 @@ public class PGM {
         this.pixels = new int[dimX][dimY];
     }
     
+    public PGM(PGM image){
+        this.dimX = image.getDimX();
+        this.dimY = image.getDimY();
+        this.pixels = new int[dimX][dimY];
+        for(int i = 0; i < dimX; i++){
+            for(int j = 0; j < dimY; j++){
+                this.pixels[i][j] = image.getPixels()[i][j];
+            }
+        }
+    }
+    
+    public PGM seuillage(PGM image, int seuil){
+        int[][] niveaux_gris_seuilles = new int[image.getDimX()][image.getDimY()];
+        
+        for(int i = 0; i < dimX; i++){
+            for(int j = 0; j < dimY; j++){
+                if(image.getPixels()[i][j] < seuil){
+                    niveaux_gris_seuilles[i][j] = 0;
+                }
+                else{
+                    niveaux_gris_seuilles[i][j] = 255;
+                }
+            }
+        }
+        
+        PGM new_image = new PGM(image.getDimX(), image.getDimY());
+        new_image.setPixels(niveaux_gris_seuilles);
+        
+        return new_image;
+    }
     
 }
