e9a9ac65b0bfba5d9d549464c7dcbdb801e0bca4
Boulanger <alexandre-boulanger@bbox.fr>
2021-12-02 10:45:35 +0100

    Ajout de GUIReduction, pareil que pour GUISeuillage, il manque les méthodes statiques de PGM

---
+++ Medev3/src/main/java/edu/centralenantes/medev3/GUIReduction.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.medev3;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author Boulanger
+ */
+public class GUIReduction extends JDialog {
+        //private PGM imageOld;
+    //private PGM imageNew;
+    private String path;
+    private JTextField seuilText;
+    private JLabel imageLabel;
+    
+    public GUIReduction(GUIPgm pgm){
+        super(pgm,"Réduction d'une image",true);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(600,400);
+        this.setLocationRelativeTo(null);
+        JPanel contentPane = (JPanel)this.getContentPane();
+        contentPane.setLayout(new GridLayout(1,2));
+        
+        //  GESTION DU SEUIL    \\
+        JPanel paneSeuil= new JPanel();
+        JLabel seuilLabel = new JLabel("Définir le coeff de réduction");
+        seuilText = new JTextField("insérer un entier ici");
+        
+        JLabel txtImageLabel=new JLabel("L'image choisie est au chemin d'accès suivant :");
+        imageLabel = new JLabel(path);
+        
+       
+    
+        paneSeuil.add(seuilLabel);
+        paneSeuil.add(seuilText);
+        paneSeuil.add(txtImageLabel);
+        paneSeuil.add(imageLabel);    
+        // GESTION DU CHARGEMENT DE L IMAGE \\
+        
+        JButton imageButton = new JButton("Selection de l'image");
+        imageButton.addActionListener((e)->ajoutImage());
+        
+        JButton enregistrerButton = new JButton("Enregistrer");
+        enregistrerButton.addActionListener((e)->Enregistrer());
+        
+        JButton appliquerSeuillage = new JButton("Lancer la réduction");
+        appliquerSeuillage.addActionListener((e)->Seuillage());
+        JPanel paneButton = new JPanel();
+        paneButton.add(imageButton);
+        paneButton.add(enregistrerButton);
+        paneButton.add(appliquerSeuillage);
+        
+        contentPane.add(paneSeuil);
+        contentPane.add(paneButton);
+        this.setVisible(true);
+    }
+    
+    private void ajoutImage(){
+        JFileChooser choix = new JFileChooser("Choisir une image");
+        FileFilter imagesFilter = new FileNameExtensionFilter("Images","pgm");
+        choix.addChoosableFileFilter(imagesFilter);
+        int retour = choix.showOpenDialog(this);
+
+        if(retour ==JFileChooser.APPROVE_OPTION){
+            //Chargement de l'image
+            path=choix.getSelectedFile().getAbsolutePath();
+            imageLabel.setText(path);
+            //image=PGM.lecture(choix.getSelectedFile().getAbsolutePath());
+        }
+    }
+    
+    private void Enregistrer(){
+        String inputValue = JOptionPane.showInputDialog("Indiquer le nom du fichier");
+        //PGM.enregistrement(newImage,inputValue);
+    }
+    
+    
+    private void Seuillage(){
+        //imageNew=PGM.reduction(imageOld,Integer.parseInt(seuilText.getText()));
+    }
+}
