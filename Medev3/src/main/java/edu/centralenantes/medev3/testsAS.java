78852a4a73aab7b875782116454daf2dd0f79a15
Anne-So <anne-sophie.jourlin@eleves.ec-nantes.fr>
2021-12-02 11:17:18 +0100

    Feature tested and corrected

---
+++ Medev3/src/main/java/edu/centralenantes/medev3/testsAS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.medev3;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author asjou
+ */
+public class testsAS {
+    public static void main(String[] args) throws IOException {
+        PGM pgm = PGM.lecture("C:\\Users\\asjou\\Documents\\Documents\\Info\\MEDEV\\Travail avec Netbeans-20211202\\Images Test PGM\\coins.pgm");
+        System.out.println(pgm.getDimX());
+        System.out.println(pgm.getDimY());
+        System.out.println(pgm.getPixels()[0][0]);
+        System.out.println(pgm.getPixels()[1][0]);
+        PGM.enregistrement(pgm, "TestCoin.pgm");
+    }
+}
