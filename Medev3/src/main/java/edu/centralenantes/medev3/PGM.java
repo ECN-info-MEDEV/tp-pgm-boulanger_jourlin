069eb4cd8f6055ef09e7e8a24f6ad8e1a62357bc
Boulanger <alexandre-boulanger@bbox.fr>
2021-12-06 18:46:48 +0100

    correction de l'histogramme du au pull

--- Medev3/src/main/java/edu/centralenantes/medev3/PGM.java
+++ Medev3/src/main/java/edu/centralenantes/medev3/PGM.java
@@ -165,7 +165,7 @@ public class PGM {
         //Lignes indiquant le type de fichier
         bw.write("P2\n#\n");
         //Entrer la taille de l'image
-        bw.write(pgm.getDimX() + " " + pgm.getDimY() + "\n");
+        bw.write(pgm.getDimY() + " " + pgm.getDimX() + "\n");
         //Le plus sombre :
         bw.write("255\n");
         //Entrer les lignes, en veillant à la contrainte de 70 caractères
